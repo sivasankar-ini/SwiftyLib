dad9c6a6f86deec0bedf77a980b841da6f11ba52
Sivasankar <107840729+sivasankarpr@users.noreply.github.com>
2025-06-25 13:36:54 +0530

    url updated

--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Mobilisten",
-            url: "https://github.com/sivasankar-ini/SwiftyLib/blob/main/MySDK.zip", checksum: "8f4ee26576ff52456a9812d796aba260a403c078a7ff8bcc19eecf5beeb0dc2e")
+            url: "https://raw.githubusercontent.com/sivasankar-ini/SwiftyLib/main/MySDK.zip", checksum: "8f4ee26576ff52456a9812d796aba260a403c078a7ff8bcc19eecf5beeb0dc2e")
     ]
 )

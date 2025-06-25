e95a7298d3dcf193a25ca4afc2472b0bf8628775
Sivasankar <107840729+sivasankarpr@users.noreply.github.com>
2025-06-25 13:30:09 +0530

    checksum added

--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Mobilisten",
-            url: "https://raw.githubusercontent.com/zoho/SalesIQ-Mobilisten-iOS/v\(version)/Mobilisten.zip", checksum: "a491872eb39d7a062694f384def4fd0fd7730ea6c390f35917980a1925d91c09")
+            url: "https://github.com/sivasankar-ini/SwiftyLib/blob/main/MySDK.zip", checksum: "8f4ee26576ff52456a9812d796aba260a403c078a7ff8bcc19eecf5beeb0dc2e")
     ]
 )

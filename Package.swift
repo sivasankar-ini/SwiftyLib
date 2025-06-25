3f0b9d78b10a2a96fa38757f30ed9ecb60e4b375
Sivasankar <107840729+sivasankarpr@users.noreply.github.com>
2025-06-25 13:40:19 +0530

    additional f/w added

--- Package.swift
+++ Package.swift
@@ -13,6 +13,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Mobilisten",
+            url: "https://raw.githubusercontent.com/sivasankar-ini/SwiftyLib/main/MySDK.zip", checksum: "8f4ee26576ff52456a9812d796aba260a403c078a7ff8bcc19eecf5beeb0dc2e"),
+        .binaryTarget(
+            name: "MobilistenCore",
+            url: "https://raw.githubusercontent.com/sivasankar-ini/SwiftyLib/main/MySDK.zip", checksum: "8f4ee26576ff52456a9812d796aba260a403c078a7ff8bcc19eecf5beeb0dc2e"),
+        .binaryTarget(
+            name: "MobilistenCallsCore",
             url: "https://raw.githubusercontent.com/sivasankar-ini/SwiftyLib/main/MySDK.zip", checksum: "8f4ee26576ff52456a9812d796aba260a403c078a7ff8bcc19eecf5beeb0dc2e")
     ]
 )
